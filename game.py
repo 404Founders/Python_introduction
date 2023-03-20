668aee451ea3192b83a62c7cb8440e0a012d7558
Martin Feineis <martifein@gmail.com>
2023-03-20 16:06:20 -0400

    adding more game files 2

---
+++ game.py
@@ -0,0 +1,16 @@
+print("You are standing at the cross section of Imperial Avenue and LaHarr Boulevard on the city planet Coruscant.")
+print("On your right side is an elevator leading to the lower levels.")
+choice = input("""Do you want to:  
+        1.) Use the elevator to go to the lower levels
+        2.) Go up north following Imperial Avenue, or
+        3.) Follow LaHarr Boulevard going west? \n""")
+#print(choice)
+if choice == 1:
+    print("""The Elevator takes you to the lower levels of Coruscant. 
+    No sunlight comes here. Only artifical light sources illuminate the Bars, Shops and Businesses around you.""")
+elif choice ==2:
+    print("""You are walking north entering the Administration district. You pass the Kashyyk Embassy,
+    a big building in a living tree and the Mon Calamari Embassy a floating bubble of water held together
+    by artificial gravity fields.""")
+else:
+    print("""LaHarr Boulevard takes you the Musuems district. You see the Alderaanian Musuem of contemporary Arts.""")
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello World")
+name = input("What's your name? ")
+if name == "Martin":
+    print("Hello Teacher " + name)
+elif name == "John":
+    print("Hello Student " + name)
+else:
+    print("Hello " + name)
